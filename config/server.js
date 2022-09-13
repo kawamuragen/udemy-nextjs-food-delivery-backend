eee3ee927bcc32176d8c64c49a42669f1edb9741
kawamura.gen <kawamura.gen@jp.fujitsu.com>
2022-09-13 17:18:53 +0900

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9adfee025b492376a173400f972519de'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6cb7ffa5-2667-4739-aee9-d1f2272e7e74'
+};
